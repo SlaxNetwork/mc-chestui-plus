9e582fa8600bb9f2763526e3fe30912d6e06fa33
Tech <debitcardzpaypal@gmail.com>
2023-03-25 13:27:44 -0400

    feat: add custom model data

--- src/main/kotlin/me/tech/mcchestui/GUIItem.kt
+++ src/main/kotlin/me/tech/mcchestui/GUIItem.kt
@@ -6,7 +6,10 @@
  */
 package me.tech.mcchestui
 
+import com.destroystokyo.paper.profile.PlayerProfile
+import net.kyori.adventure.text.Component
 import org.bukkit.Material
+import org.bukkit.OfflinePlayer
 import org.bukkit.enchantments.Enchantment
 import org.bukkit.inventory.ItemFlag
 import org.bukkit.inventory.ItemStack
@@ -29,23 +32,23 @@ class GUIItem(
 	fun meta(builder: ItemMeta.() -> Unit) =
 		itemMeta?.apply(builder)?.also { stack.itemMeta = it }
 
-	var name
+	var name: Component?
 		get() = itemMeta.displayName()
 		set(value) {
 			stack.itemMeta.apply { displayName(value) }.also { stack.itemMeta = it }
 		}
 
-	var lore
+	var lore: MutableList<Component>?
 		get() = itemMeta.lore()
 		set(value) {
 			stack.itemMeta.apply { lore(value) }.also { stack.itemMeta = it }
 		}
 
-	var amount
+	var amount: Int
 		get() = stack.amount
 		set(value) { stack.amount = value }
 
-	var skullOwner
+	var skullOwner: OfflinePlayer?
 		get() = (itemMeta as? SkullMeta)?.owningPlayer
 		set(value) {
 			(itemMeta as? SkullMeta)
@@ -53,7 +56,7 @@ class GUIItem(
 				?.also { stack.itemMeta = it }
 		}
 
-	var playerProfile
+	var playerProfile: PlayerProfile?
 		get() = (itemMeta as? SkullMeta)?.playerProfile
 		set(value) {
 			(itemMeta as? SkullMeta)
@@ -61,7 +64,7 @@ class GUIItem(
 				?.also { stack.itemMeta = it }
 		}
 
-	var glowing = false
+	var glowing: Boolean = false
 		set(value) {
 			// Add glow.
 			if(value) {
@@ -78,5 +81,13 @@ class GUIItem(
 			}
 
 			field = value
+		}
+
+	var customModelData: Int
+		get() = itemMeta.customModelData
+		set(value) {
+			stack.itemMeta.apply {
+				setCustomModelData(value)
+			}.also { stack.itemMeta = it }
 		}
 }
